7a2e6d55dfcd301ad0c8e4d04c49cf5d7ddf318b
Nevin <nevinthomas800@gmail.com>
2023-05-25 16:09:41 +0530

    Change 3

--- src/components/Add.jsx
+++ src/components/Add.jsx
@@ -1,45 +1,43 @@
 import { Button, TextField } from '@mui/material'
 import React, { useState } from 'react'
 import Student from './Student'
+import axios from 'axios'
 
 const Add = () => {
 
-  var [val,setVal] = useState("")
-  var [val1,setVal1] = useState("")
-  var [tabval,setTab] = useState("")
-  var [tabval1,setTab1] = useState("")
+  
+  var [datas,setDatas] = useState({sname:'',sgrade:''}) //Name of each textfield
+  
 
-  const inputHandler1 = (event) =>
-
-  {
-  setVal(event.target.value)
-  }
-  const inputHandler2 = (event) =>
+  const inputHandler = (e) =>
 
   {
-  setVal1(event.target.value)
+    const {name,value} = e.target
+     //Input box type in target, ee name ulladath value save in setDatas
+    setDatas((datas)=>({...datas,[name]:value})) //Datas multiple, onn add matteth povilla
+    console.log(datas)
+
+  }
+  const addValues = () =>{
+    console.log("Clicked")
+    axios.post("http://localhost:8080/create",datas)
+    .then((response)=>{
+      console.log("data",response.data)
+      alert("Successfully added data!")
+    })
+    .catch(err=>console.log(err))
   }
 
-
-  const buttonChange = () =>
-  {
-    setTab(val)
-    setTab1(val1)
-    console.log(tabval)
-    console.log(tabval1)
-    
-
-    
-  }
+ 
 
 
 
   return (
-    <div>
+    <div style={{paddingTop:"100px"}}>
       
-      <h1>Name:<TextField id="filled-basic" label="Enter Name" variant="filled" onChange={inputHandler1}/></h1> <br />
-      <h1>Grade:<TextField id="filled-basic" label="Enter Grade" variant="filled" onChange={inputHandler2}/></h1> <br />
-      <Button variant="contained" color="success" onClick={buttonChange}>Submit</Button>
+      <h1>Name:<TextField id="filled-basic" name='namev' value={datas.sname} namelabel="Enter Name" variant="filled" onChange={inputHandler}/></h1> <br />
+      <h1>Grade:<TextField id="filled-basic" name='namev' value={datas.sgrade} label="Enter Grade" variant="filled" onChange={inputHandler}/></h1> <br />
+      <Button variant="contained" color="success" onClick={addValues} >Submit</Button>
       
       
 
